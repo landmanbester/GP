567436c3ec0d7aa8cbf063b8c0a80797fc8844dd
landmanbester <lbester@ska.ac.za>
2017-06-02 17:35:37 +0200

    Added generic function to create vectorized differences in N dimensions

---
+++ tools/abs_diff.py
@@ -0,0 +1,27 @@
+"""
+Computes the vectorised differences x[i] - xp[j] in arbitrary dimensions
+"""
+
+import numpy as np
+
+def abs_diff(x,xp):
+    """
+    Gets vectorised differences between x and xp
+    :param x: NxD array of floats (inputs1)
+    :param xp: NpxD array of floats (inputs2)
+    """
+    try:
+        N, D = x.shape
+        Np = xp.shape[0]
+    except:
+        N = x.size
+        D = 1
+        Np = xp.shape[0]
+        x = np.reshape(x, (N, D))
+        xp = np.reshape(xp, (Np, D))
+    xD = np.zeros([D, N, Np])
+    xpD = np.zeros([D, N, Np])
+    for i in xrange(D):
+        xD[i] = np.tile(x[:, i], (Np, 1)).T
+        xpD[i] = np.tile(xp[:, i], (N, 1))
+    return np.linalg.norm(xD - xpD, axis=0)
