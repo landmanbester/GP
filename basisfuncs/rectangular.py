Fixed bug in eigenvals computation. Added utility to draw prior samps

basisfuncs/rectangular.py
@@ -20,4 +20,4 @@ def Lambda(j, L):
     :param j: Dx1 array of integers (order of the basis function in the summation)
     :param L: Dx1 array of floats (boundary of domain)
     """
-    return np.sum(j*np.pi/(2*L)**2)
+    return np.sum((j*np.pi/(2*L))**2)

tools/abs_diff.py
@@ -16,7 +16,7 @@ def abs_diff(x,xp):
     except:
         N = x.size
         D = 1
-        Np = xp.shape[0]
+        Np = xp.size
         x = np.reshape(x, (N, D))
         xp = np.reshape(xp, (Np, D))
     xD = np.zeros([D, N, Np])