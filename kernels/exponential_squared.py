Added functionality to give diagonal noise covariance and fixed bug in marginal posterior

kernels/exponential_squared.py
@@ -6,13 +6,14 @@ import numpy as np
 
 
 class sqexp(object):
-    def __init__(self, p=None, D=1):
+    def __init__(self, p=None, D=1, Sigmay=None):
         # p is not relevant to sq exp
         # Check that D is an integer
         if not isinstance(D, (int, long)):
             print "Converting D to integer"
             D = int(D)
         self.D = D
+        self.Sigmay = Sigmay
 
 
     def cov_func(self, theta, x, noise=True):
@@ -23,7 +24,10 @@ class sqexp(object):
         :param noise: whether to add noise or not
         """
         if noise:
-            return theta[0] ** 2.0 * np.exp(-x ** 2.0 / (2.0 * theta[1] ** 2.0)) + theta[2] ** 2.0 * np.eye(x.shape[0])
+            if self.Sigmay is not None:
+                return theta[0] ** 2.0 * np.exp(-x ** 2.0 / (2.0 * theta[1] ** 2.0)) + theta[2] ** 2.0 * self.Sigmay
+            else:
+                return theta[0] ** 2.0 * np.exp(-x ** 2.0 / (2.0 * theta[1] ** 2.0)) + theta[2] ** 2.0 * np.eye(x.shape[0])
         else:
             return theta[0] ** 2.0 * np.exp(-x ** 2.0 / (2.0 * theta[1] ** 2.0))
 
@@ -39,7 +43,10 @@ class sqexp(object):
         elif mode == 1:
             return x ** 2 * K / theta[1] ** 3
         elif mode == 2:
-            return 2 * theta[2] * np.eye(x.shape[0])
+            if self.Sigmay is not None:
+                return 2 * theta[2] * self.Sigmay
+            else:
+                return 2 * theta[2] * np.eye(x.shape[0])
 
     # def spectral_density(self, theta, s):
     #     """

kernels/mattern.py
@@ -7,7 +7,7 @@ from scipy.special import gamma
 from scipy.misc import factorial
 
 class mattern(object):
-    def __init__(self, p=3, D=1):
+    def __init__(self, p=3, D=1, Sigmay=None):
         # Check that p is an integer
         if not isinstance(p, (int, long)):
             print "Converting p to integer"
@@ -27,47 +27,67 @@ class mattern(object):
 
         # And for the spectral density
         self.preSterm = 2**D*np.pi**(D/2.0)*gamma(self.v + D/2.0)*(2*self.v)**self.v/gamma(self.v)
+        self.Sigmay = Sigmay
 
 
+
+    # def cov_func(self, theta, x, noise=True):
+    #     """
+    #     Covariance function possibly including noise variance
+    #     :param theta: hypers
+    #     :param x: the inputs x[i] - x[j] (usually matrix of differences)
+    #     :param noise: whether to add noise or not
+    #     """
+    #     tmp = np.tile(2 * self.root2v * x.flatten() / theta[1], (self.p + 1, 1)).T ** (self.p - self.I)
+    #     K = (theta[0]**2.0*np.exp(-self.root2v*x.flatten()/theta[1])*self.gamma_term*np.sum(self.factorial_arr*tmp,
+    #                                                                            axis=1)).reshape(x.shape)
+    #     if noise:
+    #         return K + theta[2] ** 2.0 * np.eye(x.shape[0])
+    #     else:
+    #         return K
 
     def cov_func(self, theta, x, noise=True):
-        """
-        Covariance function possibly including noise variance
-        :param theta: hypers
-        :param x: the inputs x[i] - x[j] (usually matrix of differences)
-        :param noise: whether to add noise or not
-        """
-        tmp = np.tile(2 * self.root2v * x.flatten() / theta[1], (self.p + 1, 1)).T ** (self.p - self.I)
-        K = (theta[0]**2.0*np.exp(-self.root2v*x.flatten()/theta[1])*self.gamma_term*np.sum(self.factorial_arr*tmp,
-                                                                               axis=1)).reshape(x.shape)
-        if noise:
-            return K + theta[2] ** 2.0 * np.eye(x.shape[0])
-        else:
-            return K
-
-    def cov_func2(self, theta, x, noise=True):
         if not noise:
             return theta[0] ** 2 * np.exp(-np.sqrt(5) * np.abs(x) / theta[1]) * (1 + np.sqrt(5) * np.abs(x) / theta[1] + 5 * np.abs(x) ** 2 / (3 * theta[1] ** 2))
         else:
-            return theta[0] ** 2 * np.exp(-np.sqrt(5) * np.abs(x) / theta[1]) * (
-            1 + np.sqrt(5) * np.abs(x) / theta[1] + 5 * np.abs(x) ** 2 / (3 * theta[1] ** 2)) + theta[2]**2.0*np.eye(x.shape[0])
+            if self.Sigmay is not None:
+                return theta[0] ** 2 * np.exp(-np.sqrt(5) * np.abs(x) / theta[1]) * (
+                    1 + np.sqrt(5) * np.abs(x) / theta[1] + 5 * np.abs(x) ** 2 / (3 * theta[1] ** 2)) + theta[2]**2.0*self.Sigmay
+            else:
+                return theta[0] ** 2 * np.exp(-np.sqrt(5) * np.abs(x) / theta[1]) * (
+                    1 + np.sqrt(5) * np.abs(x) / theta[1] + 5 * np.abs(x) ** 2 / (3 * theta[1] ** 2)) + theta[2]**2.0*np.eye(x.shape[0])
 
     def dcov_func(self, theta, x, K, mode=0):
         """
         Derivates of the covariance function w.r.t. the hyperparameters
-        :param theta: hypers
-        :param x: the inputs x[i] - x[j] (usually matrix of differences)
-        :param K: the value of the covariance function
-        :param mode: use to determine which hyper we are taking the derivative w.r.t.
         """
         if mode == 0:
-            return 2 * K / theta[0]
+            return 2*K/theta[0]
         elif mode == 1:
-            tmp = np.tile(2 * self.root2v * x.flatten() / theta[1], (self.p + 1, 1)).T ** (self.p - self.I)
-            return self.root2v*x*K/theta[1]**2 + (theta[0]**2.0*np.exp(-self.root2v*x.flatten()/theta[1])*self.gamma_term*\
-                        np.sum(-self.factorial_arr*(self.p - self.I)*tmp/theta[1], axis=1)).reshape(x.shape)
+            return np.sqrt(5)*np.abs(x)*K/theta[1]**2 + theta[0] ** 2 * \
+                        np.exp(-np.sqrt(5) * np.abs(x) / theta[1])*(-np.sqrt(5) * np.abs(x) / theta[1]**2 - 10 * np.abs(x) ** 2 / (3 * theta[1] ** 3))
         elif mode == 2:
-            return 2 * theta[2] * np.eye(x.shape[0])
+            if self.Sigmay is not None:
+                return 2 * theta[2] * self.Sigmay
+            else:
+                return 2 * theta[2] * np.eye(x.shape[0])
+
+    # def dcov_func(self, theta, x, K, mode=0):
+    #     """
+    #     Derivates of the covariance function w.r.t. the hyperparameters
+    #     :param theta: hypers
+    #     :param x: the inputs x[i] - x[j] (usually matrix of differences)
+    #     :param K: the value of the covariance function
+    #     :param mode: use to determine which hyper we are taking the derivative w.r.t.
+    #     """
+    #     if mode == 0:
+    #         return 2 * K / theta[0]
+    #     elif mode == 1:
+    #         tmp = np.tile(2 * self.root2v * x.flatten() / theta[1], (self.p + 1, 1)).T ** (self.p - self.I)
+    #         return self.root2v*x*K/theta[1]**2 + (theta[0]**2.0*np.exp(-self.root2v*x.flatten()/theta[1])*self.gamma_term*\
+    #                     np.sum(-self.factorial_arr*(self.p - self.I)*tmp/theta[1], axis=1)).reshape(x.shape)
+    #     elif mode == 2:
+    #         return 2 * theta[2] * np.eye(x.shape[0])
 
     def spectral_density(self, theta, s):
         """

tools/marginal_posterior.py
@@ -50,6 +50,19 @@ class evidence(object):
             Ky = self.kernel.cov_func(theta, self.XX, noise=True)
             # tmp is L
             try:
+                L = np.linalg.cholesky(Ky)
+            except:
+                print "Had to add jitter, theta = ", theta
+                F = True
+                while F:
+                    jit = 1e-6
+                    try:
+                        L = np.linalg.cholesky(Ky + jit*np.eye(self.N))
+                        F = False
+                    except:
+                        jit *=10.0
+                        F = True
+            try:
                 tmp = np.linalg.cholesky(Ky)
             except:
                 logp = 1.0e8
@@ -60,7 +73,7 @@ class evidence(object):
             tmp = np.linalg.inv(tmp)
             # tmp2 is Linvy
             tmp2 = np.dot(tmp, self.yDat)
-            logp = np.dot(tmp2.T, tmp2) / 2.0 + detK / 2.0 + self.N * np.log(2 * np.pi) / 2.0
+            logp = np.dot(tmp2.conj().T, tmp2).real / 2.0 + detK / 2.0 + self.N * np.log(2 * np.pi) / 2.0
             nhypers = theta.size
             dlogp = np.zeros(nhypers)
             # tmp is Kinv
@@ -68,14 +81,15 @@ class evidence(object):
             # tmp2 becomes Kinvy
             tmp2 = np.reshape(np.dot(tmp, self.yDat), (self.N, 1))
             # tmp2 becomes aaT
-            tmp2 = np.dot(tmp2, tmp2.T)
+            tmp2 = (np.dot(tmp2, tmp2.conj().T)).real
             # tmp2 becomes Kinv - aaT
             tmp2 = tmp - tmp2
-            dKdtheta = self.kernel.dcov_func(theta, self.XX, Ky, mode=0)
+            K = self.kernel.cov_func(theta, self.XX, noise=False)
+            dKdtheta = self.kernel.dcov_func(theta, self.XX, K, mode=0)
             dlogp[0] = np.sum(np.einsum('ij,ji->i', tmp2, dKdtheta)) / 2.0 #computes only the diagonal matrix product
-            dKdtheta = self.kernel.dcov_func(theta, self.XX, Ky, mode=1)
+            dKdtheta = self.kernel.dcov_func(theta, self.XX, K, mode=1)
             dlogp[1] = np.sum(np.einsum('ij,ji->i', tmp2, dKdtheta)) / 2.0
-            dKdtheta = self.kernel.dcov_func(theta, self.XX, Ky, mode=2)
+            dKdtheta = self.kernel.dcov_func(theta, self.XX, K, mode=2)
             dlogp[2] = np.sum(np.einsum('ij,ji->i', tmp2, dKdtheta)) / 2.0
             return logp, dlogp
         elif self.mode == "RR":